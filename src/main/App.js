37961d9fa8ba627411710801b39df994d9aaae9f
hidekiabe <hideki-abe@hotmail.com>
2023-02-06 02:56:03 -0300

    configuração inicial

---
+++ src/main/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '~bootswatch/dist/vapor/variables';
+import '~bootstrap/scss/bootstrap';
+import '~bootswatch/dist/vapor/bootswatch';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
