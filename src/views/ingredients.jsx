770334f7e30dbe072f96948ad37418d468f98ef9
hidekiabe <hideki-abe@hotmail.com>
2023-02-06 05:41:35 -0300

    Implantação: card de receita

--- src/views/ingredients.jsx
+++ src/views/ingredients.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function RegisterIngredient(props) {
+export default function Ingredients(props) {
     return(
         <>
             <h1>Registro de Ingredientes.</h1>
